4192930d71d694b2add02edec7b12542da7951b9
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-16 20:47:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  arr = []
+  
 end
 
 def long_planeteer_calls# code an argument here
